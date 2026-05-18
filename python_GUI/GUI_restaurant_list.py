b11f8734d04344a7759d2e1307f88d43a471d4a0
03rldnjs <03rldnjs@gmail.com>
2026-05-19 08:57:39 +0900

    Enhance GUI_restaurant_list.py with comments
    Added comments in Korean to explain the functionality of the GUI restaurant list application.

--- python_GUI/GUI_restaurant_list.py
+++ python_GUI/GUI_restaurant_list.py
@@ -1,17 +1,19 @@
 from tkinter import *
 
-window = Tk()
+window = Tk()  # tkinter에 들어있는 클래스를 window라는 이름의 객체로 생성시킴 / create instance of 'class Tk()' which is included in tkinter library
 
-restaurants = {}
+restaurants = {}  # 공백 딕셔너리 생성 / create empty dictionary
 
+# 식당 이름 검색 함수 / method that can search restaurant's name in dictionary
 def search_list():
-    rest_name = e1.get()
+    rest_name = e1.get()  
     if rest_name in restaurants:
         l3.config(text = f'식당 이름: {rest_name}, 대표 메뉴: {restaurants[rest_name]}')
         e1.delete(0, END)
     else:
         l3.config(text = '검색하신 식당은 리스트에 존재하지 않습니다.')
-    
+
+# 식당 추가 함수 / method that can add new restaurant
 def add_list():
     if len(e2.get()) == 0 or len(e3.get()) == 0:
         l3.config(text = '식당 이름과 메뉴를 모두 입력해주세요')
@@ -24,20 +26,21 @@ def add_list():
     e2.delete(0, END)
     e3.delete(0, END)
 
+# 식당 리스트 출력 함수 / method that can print list of restaurants
 def view_list():
     # l3.config(text = f'{restaurants}')  처음에 사용했던 원시적인 방법
-    output_text = ""
+    output_text = ""  # 공백 문자열 생성 / create empty string
 
-    if not restaurants:
+    if not restaurants:  # 딕셔너리가 공백인 경우 출력할 내용 / print warning sign when dictionary is empty
         l3.config(text = '저장된 식당이 없습니다.')
         return
     
-    for rest, men in restaurants.items():
-        output_text += f'식당 이름: {rest} | 대표 메뉴: {men}\n'
+    for rest, men in restaurants.items():  # items()함수를 활용하여 식당 이름과 메뉴를 각각 언패킹 / unpack the keys and values respectly by function 'items()'
+        output_text += f'식당 이름: {rest} | 대표 메뉴: {men}\n'  # 식당 이름과 대표 메뉴를 줄바꿈 문자를 포함하여 text에 추가 / add restaurant' name and menu with \n in text
 
-    l3.config(text = output_text)
+    l3.config(text = output_text)  # .config()는 줄바꿈 문자도 잘 표현함 / .config() can express \n very well
 
-window.title("맛집 리스트")
+window.title("맛집 리스트") 
 
 l_first = Label(window, text = '식당 검색')
 l_first.grid(row = 0, column = 0)
