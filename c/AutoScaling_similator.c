2011434db09f277c123e448f760eee4a4213376a
03rldnjs <03rldnjs@gmail.com>
2026-09-02 11:14:03 +0900

    Enhance code comments for better readability
    Added comments for clarity and understanding of the code logic.

--- c/AutoScaling_similator.c
+++ c/AutoScaling_similator.c
@@ -49,14 +49,15 @@ int main() {
 		scanf("%d", &count);
 	}
   
-  // malloc을 통한 동적 메모리 할당(count의 개수만큼 구조체 공간이 생성됨)
+  	// malloc을 통한 동적 메모리 할당(count의 개수만큼 구조체 공간이 생성됨)
 	Instance* servers = (Instance*)malloc(sizeof(Instance) * count);
-  // 메모리 할당이 실패할 경우를 대비한 코드(defensive programming)
+  	// 메모리 할당이 실패할 경우를 대비한 코드(defensive programming)
 	if (servers == NULL) {
 		printf("초기 메모리 할당 실패!\n");
 		return 1;
 	}
-
+	// 인스턴스 ID 및 CPU 사용량 입력부
+	// input the instance ID & CPU usage
 	for (int i = 0; i < count; i++) {
 		printf("\n[%d번 인스턴스] ID 입력: ", i + 1);
 		scanf("%19s", servers[i].id);
@@ -65,17 +66,25 @@ int main() {
 		scanf("%d", &servers[i].cpu_usage);
 	}
 
+	// avg라는 변수 정의 + 해당 변수에 get_average_cpu 함수의 return값을 저장
+	// define 'avg' variable + store the return value of get_average_cpu function in 'avg'
 	double avg = get_average_cpu(servers, count);
 	printf("\n현재 전체 평균 CPU 사용량: %.2f%%\n", avg);
 
+	// 평균 사용량이 적정 수치를 넘었을 때 인스턴스 확장 알고리즘
+	// scale out the instance when CPU average usage over the threshold
 	while (avg >= 70.0) {
 		printf("\n[ALERT] 평균 CPU 사용량 70%% 초과\n");
+		// 적정 평균 사용량 초과 상태이나, 인스턴스수가 최대 인스턴스 수에 도달한 경우
+		// the average usage already over the threshold, but number of instance reach the max instance number
 		if (count >= MAX_CAPACITY) {
 			printf("적정 평균 사용량을 초과하였으나, 최대 인스턴스 수에 도달하여 더 확장할 수 없습니다.\n");
 			break;
 		}
 		printf("Auto Scaling(Scale Out)을 진행합니다.\n");
 
+		// 늘어난 인스턴스 수에 맞게 realloc
+		// realloc according to the increased number of instances
 		Instance* temp = (Instance*)realloc(servers, sizeof(Instance) * (count + 1));
 		if (temp != NULL) {
 			servers = temp;
@@ -85,6 +94,8 @@ int main() {
 			printf("메모리 확장 실패!\n");
 			break;
 		}
+		// 추가할 인스턴스에 대한 정보 입력
+		// input the imformations of new instance
 		printf("\n[%d번 인스턴스] ID 입력: ", count);
 		scanf("%19s", servers[count - 1].id);
 		printf("[%d번 인스턴스] CPU 사용량 입력: ", count);
@@ -92,14 +103,20 @@ int main() {
 		avg = get_average_cpu(servers, count);
 		printf("\n현재 전체 평균 CPU 사용량: %.2f%%\n", avg);
 	}
+	// 평균 사용량이 적정 수치 미만일 때 인스턴스 축소 알고리즘
+	// scale in the instance numbers when average usage below the minimum rate
 	while (avg <= 30.0) {
 		printf("\n[ALERT] 평균 CPU 사용량 30%% 미만\n");
+		// 적정 평균 사용량 미만 상태이나, 인스턴스수가 최소 인스턴스 수에 도달한 경우
+		// the average usage already below the threshold, but number of instance reach the min instance number
 		if (count <= MIN_CAPACITY) {
 			printf("적정 평균 사용량 미만이나, 최소 인스턴스 수에 도달하여 더 축소할 수 없습니다.\n");
 			break;
 		}
 		printf("Auto Scaling(Scale IN)을 진행합니다.\n");
 
+		// 줄어든 인스턴스 수에 맞게 realloc
+		// realloc according to the decreased number of instances
 		Instance* temp = (Instance*)realloc(servers, sizeof(Instance) * (count - 1));
 		if (temp != NULL) {
 			servers = temp;
@@ -113,6 +130,8 @@ int main() {
 		printf("\n현재 전체 평균 CPU 사용량: %.2f%%\n", avg);
 		printf("현재 인스턴스 개수: %d\n", count);
 	}
+	// 최종 AutoScaling 결과 출력
+	// print the final AutoScaling result
 	printf("\n=== 최종 인스턴스 상태 목록 (총 %d대) ===\n", count);
 	for (int i = 0; i < count; i++) {
 		printf("- %s : %d%%\n", servers[i].id, servers[i].cpu_usage);
